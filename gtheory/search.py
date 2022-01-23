6a0544c42133a672dd1f88a9d983d1cbc6cde56a
MalarzDawid <github@dawidmalarz.pl>
2022-01-23 21:28:51 +0100

    Move BFS and DFS to one class

--- gtheory/search.py
+++ gtheory/search.py
@@ -1,15 +1,53 @@
-def dfs(visited, graph, at):
-    if visited[at]:
-        return
-    visited[at] = True
+class Graph:
+    def __init__(self, adj_list: dict, nodes: int) -> None:
+        self.adj_list = adj_list
+        self.nodes = nodes
+        self.visited = [False] * self.nodes
 
-    for neighbour in graph[at]:
-        dfs(visited, graph, neighbour)
-    return visited
+    def BFS(self, start: int) -> None:
+        queue = []
+        queue.append(start)
+
+        self.visited[start] = True
+
+        while queue:
+            node = queue.pop(0)
+
+            if node in self.adj_list:
+                for adj_node in self.adj_list[node]:
+                    if self.visited[adj_node] == False:
+                        queue.append(adj_node)
+                        self.visited[adj_node] = True
+                        print(adj_node, end=" ")
+        self.visited = [False] * self.nodes
+
+    def DFS(self, start: int) -> list:
+        if self.visited[start]:
+            return
+        self.visited[start] = True
+        neighbours = self.adj_list[start]
+        for neighbour in neighbours:
+            self.DFS(start=neighbour)
+        return self.visited
+
+    def display(self) -> None:
+        for node in self.adj_list.items():
+            print(node)
 
 
 if __name__ == "__main__":
-    graph = [[1, 2], [0, 3, 4], [0], [1], [2, 3]]
-    visited = [False, False, False, False, False]
-    output = dfs(visited, graph, 0)
-    print(output)
+    adj_list = {0: [1, 2],
+                1: [0, 3, 4], 
+                2: [0, 3], 
+                3: [1, 2, 5], 
+                4: [1], 
+                5: [3]}
+
+    g = Graph(adj_list, len(adj_list))
+    print("Start Node: 0")
+    g.BFS(0)
+    print("\nGraph Display: ")
+    g.display()
+    print("DFS start: ")
+    dfs_output = g.DFS(0)
+    print(dfs_output)
